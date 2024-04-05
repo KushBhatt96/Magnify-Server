fe53668401514fa9871aafdf398f2e8a45c514dd
KushBhatt96 <kush.bhatt96@yahoo.com>
2024-04-04 20:03:52 -0500

    First commit

---
+++ Magnify.Service/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Magnify.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
